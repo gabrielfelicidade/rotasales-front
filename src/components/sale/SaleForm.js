8296ced8d01166319018cce082addda875f59bff
Gabriel Felicidade <gabriel.felicidade@etec.sp.gov.br>
2022-07-12 16:53:50 -0300

    fix media width sale form

--- src/components/sale/SaleForm.js
+++ src/components/sale/SaleForm.js
@@ -43,8 +43,8 @@ const SaleForm = (props) => {
 
     const handleChangeEvent = (event) => {
         setSale({ ...sale, event: event.target.value, items: [] });
-        
-        (async() => {
+
+        (async () => {
             const eventItems = await getItemsByEventId(event.target.value);
             setItems(eventItems);
             setItemsMap(eventItems.reduce((acm, acc) => { acm[acc.id] = acc; return acm }, {}));
@@ -52,12 +52,12 @@ const SaleForm = (props) => {
     }
 
     const handleAddItem = () => {
-        setSale({ ...sale, items: [ ...sale.items, { id: '', amount: 1 } ] })
+        setSale({ ...sale, items: [...sale.items, { id: '', amount: 1 }] })
     }
 
     const handleChangeItem = (event, index) => {
         const existentItemIndex = sale.items.findIndex(item => item.id === event.target.value);
-        const newItems = [ ...sale.items ];
+        const newItems = [...sale.items];
         newItems[index] = { ...sale.items[index], id: event.target.value };
 
         if (existentItemIndex >= 0) {
@@ -75,14 +75,14 @@ const SaleForm = (props) => {
         }
         const newItem = { ...sale.items[index] };
         newItem.amount = value;
-        const newItems = [ ...sale.items ];
+        const newItems = [...sale.items];
         newItems[index] = newItem;
 
         setSale({ ...sale, items: newItems });
     }
 
     return (
-        <Grid container spacing={4}>
+        <Grid container spacing={4} style={{ justifyContent: 'center' }}>
             <Grid item xs={12} md={4}>
                 <TextField value={sale.buyer} onChange={(e) => handleChangeSale('buyer', e.target.value)} id="buyer" label="Comprador" variant="outlined" style={{ width: '100%' }} />
             </Grid>
@@ -104,7 +104,7 @@ const SaleForm = (props) => {
                     </Select>
                 </FormControl>
             </Grid> : null}
-            <Grid item xs={12} md={4} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center'  }}>
+            <Grid item xs={12} md={4} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
                 <FormControlLabel
                     label="Doação"
                     control={<Checkbox color="primary" checked={sale.donation} onChange={(e) => handleChangeSale('donation', !sale.donation)} />}
@@ -114,25 +114,28 @@ const SaleForm = (props) => {
                 const totalAmount = item.id ? item.amount * (itemsMap[item.id] ? itemsMap[item.id].value : 0) : 0;
 
                 return (
-                    <Grid item xs={12} style={{ display: 'flex', flexWrap: 'wrap', gap: '20px', alignItems: 'center', justifyContent: 'center'  }} key={`grid-item-${index}`}>
-                        <FormControl sx={{ minWidth: 80 }}
-                            style={{ width: '100%' }}>
-                            <InputLabel id="item-select">Item</InputLabel>
-                            <Select
-                                labelId="item-select"
-                                label="item"
-                                style={{ width: '100%' }}
-                                value={item.id}
-                                onChange={(e) => handleChangeItem(e, index)}
-                            >
-                                <MenuItem value={''}>Selecione um item</MenuItem>
-                                {items.map(item => <MenuItem value={item.id} key={item.id}>{item.name}</MenuItem>)}
-                            </Select>
-                        </FormControl>
-                        <TextField type="number" value={item.amount} onChange={(e) => handleChangeItemAmount(e, index)} id="amount" label="Quantidade" variant="outlined" />
-                        <TextField disabled value={formatCurrency(totalAmount)} id="total" label="Valor Total" variant="outlined" />
+                    <Grid item xs={12} style={{ display: 'flex', flexWrap: 'wrap', gap: '20px', alignItems: 'center', justifyContent: 'center' }} key={`grid-item-${index}`}>
+                        <Grid item xs={12} md={8} lg={6} style={{ display: 'flex', flexWrap: 'wrap', gap: '20px', alignItems: 'center', justifyContent: 'center' }}>
+                            <FormControl sx={{ minWidth: 80 }}
+                                style={{ width: '100%' }}>
+                                <InputLabel id="item-select">Item</InputLabel>
+                                <Select
+                                    labelId="item-select"
+                                    label="item"
+                                    style={{ width: '100%' }}
+                                    value={item.id}
+                                    onChange={(e) => handleChangeItem(e, index)}
+                                >
+                                    <MenuItem value={''}>Selecione um item</MenuItem>
+                                    {items.map(item => <MenuItem value={item.id} key={item.id}>{item.name}</MenuItem>)}
+                                </Select>
+                            </FormControl>
+                            <TextField type="number" value={item.amount} style={{ flex: '1' }} onChange={(e) => handleChangeItemAmount(e, index)} id="amount" label="Quantidade" variant="outlined" />
+                            <TextField disabled value={formatCurrency(totalAmount)} style={{ flex: '1' }} id="total" label="Valor Total" variant="outlined" />
+                        </Grid>
                     </Grid>
-            )}) : null}
+                )
+            }) : null}
             {sale.event ? <Grid item xs={12} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
                 <Button variant="contained" color="success" endIcon={<Add />} onClick={handleAddItem}>
                     Adicionar item
